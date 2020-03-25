95d0f8440f78c70a60e7b024ef3671cf15dc1f71
Alex Beeston <alexmbeeston@gmail.com>
2020-03-25 10:16:10 -0600

    supports everything in dir simple_expressions except bad idea and long expression

--- constructs/prelude/types/SimpleType.cpp
+++ constructs/prelude/types/SimpleType.cpp
@@ -14,7 +14,7 @@ SimpleType::SimpleType(std::string* p_ident)
     else if (ident->compare("STRING") == 0) typeIndicator = 2;
     else if (ident->compare("boolean") == 0) typeIndicator = 3;
     else if (ident->compare("BOOLEAN") == 0) typeIndicator = 3;
-    else throw std::runtime_error("Error: it was attempted to construct a SimpleType with the identifier" + *ident + ", which is not a primitive type");
+    else throw std::runtime_error("Error: it was attempted to construct a SimpleType with the identifier " + *ident + ", which is not a primitive type");
     size = 4;
 }
 
