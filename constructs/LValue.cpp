237d9d85137affa3b411bd1e1618c4755e5b43c7
Alex Beeston <alexmbeeston@gmail.com>
2020-04-01 09:32:25 -0600

    refactored LValue::getBaseRegister() to save a register

--- constructs/LValue.cpp
+++ constructs/LValue.cpp
@@ -25,14 +25,12 @@ Register LValue::getBaseRegister()
         ArrayType* array = dynamic_cast<ArrayType*>(entry.type);
         Register r1 = rp.getRegister();
         std::cout << "li " << r1.getName() << " " << array->low->getValue() << "   # lower bound of array\n";
-        Register r2 = rp.getRegister();
-        Register r3 = (*sequence)[1]->index->emit();
-        std::cout << "la " << r2.getName() << " (" << r3.getName()  << ")  # index of item in array\n"; // assumes that the second item (index 1) of the LValueBase array refers to an index.
+        Register r3 = (*sequence)[1]->index->emit(); // change this part!
+        std::cout << "# just loaded the index into r3\n";
+        std::cout << "sub " << r1.getName() << " " << r3.getName() << " " << r1.getName() << "   # number of elements offset from lower bound\n";
+        std::cout << "li " << r3.getName() << " " << array->type->computeSize() << "   # loaded size of underlying type of array\n";
+        std::cout << "mult " << r1.getName() << " " << r3.getName() << "\n";
         rp.returnRegister(r3);
-        std::cout << "sub " << r1.getName() << " " << r2.getName() << " " << r1.getName() << "   # number of elements offset from lower bound\n";
-        std::cout << "li " << r2.getName() << " " << array->type->computeSize() << "   # loaded size of underlying type of array\n";
-        std::cout << "mult " << r1.getName() << " " << r2.getName() << "\n";
-        rp.returnRegister(r2);
         std::cout << "mflo " << r1.getName() << "\n";
         std::cout << "add " << r1.getName() << " " << entry.baseRegister.getName() << " " << r1.getName() << "\n";
         return r1;
