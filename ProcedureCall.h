4f453bae988d4efe127a048ebf6389ab4124d249
Alex Beeston <alexmbeeston@gmail.com>
2020-04-24 16:10:19 -0600

    refactored CMake file. Still includes flex/bison, but oh well

--- ProcedureCall.h
+++
@@ -1,9 +0,0 @@
-#include "Expression.h"
-#include "Statement.h"
-#include <vector>
-
-struct ProcedureCall : Statement
-{
-    char* ident;
-    std::vector< std::vector<Expression> > expressionLists;
-};
